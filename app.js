c3a99a89bd05031ef5ab59aa828fbf594facfa4e
matheusjuda <matheusbj582@gmail.com>
2023-10-19 21:44:57 -0300

    Validação corretiva para solucionar input vazio

--- app.js
+++ app.js
@@ -17,22 +17,28 @@ function exibirMensagemInicial() {
 exibirMensagemInicial();
 
 function verificarChute() {
-    let chute = document.querySelector('input').value;
+    let chute = document.getElementById('display').value;
 
-    if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou!');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
-        exibirTextoNaTela('p', mensagemTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled');
-    } else {
-        if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número secreto é menor');
+    if (chute != "") {
+
+        if (chute == numeroSecreto) {
+            exibirTextoNaTela('h1', 'Acertou!');
+            let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+            let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+            exibirTextoNaTela('p', mensagemTentativas);
+            document.getElementById('reiniciar').removeAttribute('disabled');
         } else {
-            exibirTextoNaTela('p', 'O número secreto é maior');
+            if (chute > numeroSecreto) {
+                exibirTextoNaTela('p', 'O número secreto é menor');
+            } else {
+                exibirTextoNaTela('p', 'O número secreto é maior');
+            }
+            tentativas++;
+            limparCampo();  
         }
-        tentativas++;
-        limparCampo();
+           
+    } else {
+        exibirTextoNaTela('p', 'O campo não pode estar vazio!');
     }
 }
 
@@ -53,7 +59,7 @@ function gerarNumeroAleatorio() {
 }
 
 function limparCampo() {
-    chute = document.querySelector('input');
+    chute = document.getElementById('display');
     chute.value = '';
 }
 
@@ -64,9 +70,3 @@ function reiniciarJogo() {
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
-
-
-
-
-
-
